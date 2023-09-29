da7b68f1825127e3a74272a566e75c8729b9ac1a
Tharcisse Mukundayi <mukundayi@gmail.com>
2023-09-29 11:39:22 +0300

    odoo-0.3.06

--- charts/odoo/scripts/rclone_run.sh
+++ charts/odoo/scripts/rclone_run.sh
@@ -5,7 +5,7 @@ do
     if [[ -n ${S3BUCKET_NAME} ]] then
         for f in $(find /backup/* -type f -mmin +3)
         do
-            rclone sync -P $f ${S3PROVIDER}:${S3BUCKET_NAME}/{{.Values.namespace}}
+            rclone sync -P $f ${PROVIDER}:${BUCKET_NAME}/{{.Values.namespace}}
             rm -f $f       
         done
         sleep 5m
