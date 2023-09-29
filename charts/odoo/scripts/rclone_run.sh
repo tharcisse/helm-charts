620d655b665d44995c1ceb90b6309e264bbd4539
Tharcisse Mukundayi <mukundayi@gmail.com>
2023-09-29 12:10:29 +0300

    odoo-0.3.09

--- charts/odoo/scripts/rclone_run.sh
+++ charts/odoo/scripts/rclone_run.sh
@@ -2,7 +2,7 @@
 set -exo pipefail
 while True:
 do     
-    if [[ -n ${S3BUCKET_NAME} ]] then
+    if [[ "$BUCKET_NAME" != "undefined" && "$PROVIDER" != "undefined" ]] then
         for f in $(find /backup/* -type f -mmin +3)
         do
             rclone sync -P $f ${PROVIDER}:${BUCKET_NAME}/{{.Values.namespace}}
