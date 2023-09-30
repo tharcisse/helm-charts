aba4003a6acb2e4549e2d5e3ca23fc7444978fa3
Tharcisse Mukundayi <mukundayi@gmail.com>
2023-09-30 08:29:07 +0300

    0.3.29

--- charts/odoo/scripts/rclone_run.sh
+++ charts/odoo/scripts/rclone_run.sh
@@ -2,11 +2,11 @@
 set -exo pipefail
 while :
 do     
-    echo "BUCKET $BUCKET_NAME PROVIDER $PROVIDER"
-    if [[ "$BUCKET_NAME" != "undefined" && "$PROVIDER" != "undefined" ]]; then
+    echo "BUCKET $BUCKET_NAME PROVIDER $STNAME"
+    if [[ "$BUCKET_NAME" != "undefined" && "$STNAME" != "undefined" ]]; then
         for f in $(find /backup/* -type f -mmin +3);
         do
-            rclone sync -P $f ${PROVIDER}:${BUCKET_NAME}/{{.Values.namespace}}
+            rclone sync -P $f ${STNAME}:${BUCKET_NAME}/${ODOO_DB}
             rm -f $f       
         done
         sleep 2m
