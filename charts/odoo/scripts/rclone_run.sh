a0c1f94ef3c76bff384786eff1b5880ef5a2b5bd
Tharcisse Mukundayi <mukundayi@gmail.com>
2023-10-02 08:06:26 +0300

    0.3.41

--- charts/odoo/scripts/rclone_run.sh
+++ charts/odoo/scripts/rclone_run.sh
@@ -7,19 +7,21 @@ do
     if [[ "$BUCKET_NAME" != "undefined" && "$STNAME" != "undefined" ]]; then
         for f in $(find /backup/* -type f -mmin +2);
         do
+            filename=${f#"/backup/"}
             rclone sync -P $f ${STNAME}:${BUCKET_NAME}/${ODOO_DB}
             rm -f $f
             echo "Sent to Object Storage"  
-            curl -X POST -H "Content-Type: application/json" -d "{\"namespace\": ${ODOO_DB},\"backup_name\": ${f},\"code\": ${POD_CODE}}" ${SAAS_MANAGER_URL}/backup_notifier
+            curl -X POST -H "Content-Type: application/json" -d "{\"namespace\": \"${ODOO_DB}\",\"backup_name\": \"${filename}\",\"code\": \"${POD_CODE}\"}" ${SAAS_MANAGER_URL}/backup_notifier
         done
         for f in $(find /restore/*.to_restore);
         do
-            remote_file=
-            rclone sync -P ${STNAME}:${BUCKET_NAME}/${ODOO_DB}/${f%"to_store"}.zip /restore/temp
+            filetorestore=${f#"/backup/"}
+            filename=${filetorestore%"to_restore"}
+            rclone sync -P ${STNAME}:${BUCKET_NAME}/${ODOO_DB}/${filename}.zip /restore/temp
             mv /restore/temp/* /restore/
             rm -f $f
             echo "restored to Object Storage"       
-            curl -X POST -H "Content-Type: application/json" -d "{\"namespace\": ${ODOO_DB}, \"restore_name\": ${f},\"code\": ${POD_CODE}}" ${SAAS_MANAGER_URL}/restore_notifier
+            curl -X POST -H "Content-Type: application/json" -d "{\"namespace\": \"${ODOO_DB}\", \"restore_name\": \"${filename}\",\"code\": \"${POD_CODE}\"}" ${SAAS_MANAGER_URL}/restore_notifier
         done
         sleep 20
     fi
