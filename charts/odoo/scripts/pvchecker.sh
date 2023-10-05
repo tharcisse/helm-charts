2cbacdc1ca83761dd5bf6bc62a48719033e2dce3
Tharcisse Mukundayi <mukundayi@gmail.com>
2023-10-05 09:54:06 +0300

    odo2.0.19

--- charts/odoo/scripts/pvchecker.sh
+++ charts/odoo/scripts/pvchecker.sh
@@ -4,10 +4,12 @@ index=6
 while :
 do
     echo $(ls -l /var/lib)
+    echo $(ls -l /var/lib/odoo)
     /bin/chmod -R 777 /var/lib/odoo
     if [ -w "/var/lib/odoo" ]; then 
         echo "WRITABLE"; 
         echo $(ls -l /var/lib)
+        echo $(ls -l /var/lib/odoo)
         break;
     fi
     if [ "$index"  -le "0" ]; then
